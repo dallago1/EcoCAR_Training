f1945a12714b3536aa02a7a865b1f0ad46c09757
ddallago77 <50114591+ddallago77@users.noreply.github.com>
2022-10-19 10:41:46 -0400

    Commits for Patricks and Emmmi's git practice

---
+++ Test Folder/Patrick_Test.m
@@ -0,0 +1,9 @@
+%% Practice making plots
+x=(-100:1:1:100);
+y=4*x.^2-4=0;
+y1=x*log(10);
+y2=-x.^3-5*x.^2+x*50+3;
+
+%% Create Plots for y, y1, y2
+% Make on with subplots of indiviual figures,  make one combined graph with
+% a legeng. Also Include titles, line size, line color, and axis title. 
