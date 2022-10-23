058d013b412886d9d822238cab3f02c31cdfea74
emshimiyimana <emshimiyimana@wilberforce.edu>
2022-10-23 10:39:29 -0400

    gjhgj

--- Test Folder/Emmi_Test.m
+++ Test Folder/Emmi_Test.m
@@ -1,9 +1,22 @@
 %% Practice making plots
-x=(-100:1:1:100);
-y=4*x.^2-4=0;
+x=(-100:1:100);
+y=4*x.^2-4;
 y1=x*log(10);
 y2=-x.^3-5*x.^2+x*50+3;
 
 %% Create Plots for y, y1, y2
 % Make on with subplots of indiviual figures,  make one combined graph with
 % a legeng. Also Include titles, line size, line color, and axis title. 
+
+figure
+plot(x,y, "LineWidth",2, "color", 'r')
+ylim([-100 100])
+xlim([-100 100])
+hold on
+grid on
+plot(x,y1, "LineWidth",2, "color", 'b')
+plot(x,y2, "LineWidth",2, "color", 'k')
+title("Practice Plot")
+xlabel("time(S)")
+ylabel('height [m]')
+legend("y",'y1','y2')
